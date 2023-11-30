288256de0c5ccfc8711d3d6023ec34a308f96228
Om Shejul <contact@omshejul.com>
2023-11-30 20:54:44 +0530

    whatsapp icon

--- app/Components/ContactBtn/ContactBtn.tsx
+++ app/Components/ContactBtn/ContactBtn.tsx
@@ -5,12 +5,16 @@ import Link from "next/link";
 
 const ContactBtn = () => {
   return (
-    <>
-    <div className='fixed bottom-0 left-0 m-2 p-2 lg:m-8 bg-yellow-300 rounded-full'>
-        <Link href='tel:+917768855757'><HiPhone size={'48'} color='black' /></Link>
+    <div className='z-10 fixed bottom-0 left-0 m-2 p-2 flex lg:m-8'>
+    <div className=' p-2 m-2 bg-yellow-300 rounded-full flex'>
+        <Link href='tel:+917768855757'><HiPhone className='w-9 h-9 lg:w-6 lg:h-6' color='black' /></Link>
     </div>
-    </>
+    <div className=' p-2 m-2 bg-[#25D366] rounded-full flex'>
+        <Link href='tel:+917768855757'><FaWhatsapp className='w-9 h-9 lg:w-6 lg:h-6' color='white' /></Link>
+    </div>
+    </div>
   )
 }
 
 export default ContactBtn
+
