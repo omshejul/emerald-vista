3cb060eeb5d3afd57664f0f527bcae32ab8361ee
Om Shejul <contact@omshejul.com>
2024-01-05 19:33:07 +0530

    Add CSS styles for testimonials section slider

--- app/Components/Sections/Testimonials/Testimonials.tsx
+++ app/Components/Sections/Testimonials/Testimonials.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Heading from "../../Text/Heading";
 import ReadMore from "./ReadMore/ReadMore"
+import styles from "./Testimonials.module.css";
 
 const Testimonials = () => {
   return (
     <div className=" mb-8">
       <Heading>Testimonials</Heading>
-      <div className="flex flex-wrap justify-center max-w-7xl">
+      <div className={styles.slider}>
+
+      <div className={`${styles.slidetrack}`}>
         <Card>
-          <div className="text-xl">Asosi Pat</div>
+          <div className={`text-xl`}>Asosi Pat</div>
           <div className="star">⭐⭐⭐⭐</div>
           <br />
           <ReadMore
@@ -58,6 +61,271 @@ const Testimonials = () => {
           />
           Rooms 5.0 Service 5.0 Location 5.0
         </Card>
+        <Card>
+          <div className="text-xl">Hasti</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="I had a great stay at Emerald Vista! The host responded very quickly and was very accommodating to my requests. Check-in instructions were super detailed and helpful. 
+            The apartment was very clean and well stocked . I could not have asked for better location. Overall a wonderful place."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Radhey, Pune, India</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="The rooms are immaculate, the service is great and so is food. The best part is that the owner is very warm and extremely helpful person."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Jens, Berlin, Germany</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="The place is great to stay as it like a little flat. Basic food for breakfast, lunch and dinner is available. The room was clean and everything you need available."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Archana</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="It was a great stay @Emrald Vista. Great Hospitality, the food was too good, special thanks to Mr. Shiva and team, they all were on their toes and gave us a very good service.
+            thanks a lot!"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Deepti</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="This place is good for short trip. Though the location was bit off-track for us, but we liked the entire vibe of place. Complete value for money. Food is better than any hotel- fresh and tasty. I will rate it 4 star for cleanliness and 5 star for food and people.
+            Overall I will recommend this place to anyone who’s looking for a good, peaceful, budgetary place to spend a weekend."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Abhay</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Amazing airbnb. One of the cosiest places in Pune for stay. Checkin process was smooth, the place was really clean, outside food was allowed, the place was small but the owner knows how to make use os space and did a fantastic job of making this airbnb.
+            As a complimentery service, they provided milk powder and coffee sachets(which all Airbnb’s must have, as a service)"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Nitin Shetty</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Unbelievable luxury at such a budget. Extremely clean n hygienic room, well furnished and maintained."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Akshay Agrawal</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Everything was good, I had been there 2nd time and always it has been the best stay of pune no issue nothing"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Kristen, New York, NY</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Very nice, no-frills, multi-unit building with independent suites. Accommodating staff and very communicative and easy to reach host. Great price! Loved having the private patio and little kitchenette."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Sachin Kabade</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="I’m glad to hear you had a positive experience! consider leaving a review like this: I had a wonderful stay! the staff was helpful and cooperative. The hotel’s Location is ideal. Overall, a nice a good experience."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Torour, Aarhus, Denmark</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Its a great place away from all the noise in the big city. 
+            The host is very friendly and helpful.
+            Highly recommended!"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        {/* halfway */}
+        <Card>
+          <div className={`text-xl`}>Asosi Pat</div>
+          <div className="star">⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="
+          I stayed here for 2 weeks I must say i like that location near the high way...The staff are very kind and the place is safe ...The problem we had was that the ac had water leaked on it so in the morning the floor was always wet.. ... But overall, that place is ok for me, I will stay there again if ever I have a chance to visit India again ....🤗❤ from Philippines 🇵🇭"
+          />
+          <br />
+          Rooms4.0 Service4.0 Location5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Rohan Biswakarma</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Emerald Vista Luxury Suites truly lives up to its name, providing a remarkable experience. The corporate service apartment boasts sophisticated decor, spacious rooms, and impeccable amenities, making it a haven for business travelers. Adding to the charm is the onsite restaurant, where culinary excellence meets a cozy ambiance. The diverse menu offered delightful gastronomic experiences, and the friendly staff ensured every visit was a pleasure. From check-in to dining, the level of hospitality was exceptional. For a seamless blend of luxurious accommodation, exquisite dining, and welcoming staff, Emerald Vista Luxury Suites stands as an exemplary choice. Highly recommended for a memorable stay.
+          "
+          />
+          Rooms5.0 Service5.0 Location5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Akshay Agrawal</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Nice stay smooth check-in very friendly staff and we'll mannered. I checked in at 2 am they were still extremely friendly at that time also. Should have better food options in breakfast but still they offer you many so a very nice complete stay with spacious rooms balcony and living area
+          "
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">B S Arun</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="It is a nice hotel. The room they provided is very spacious. It doesn't feel like a typical room; it resembles a one BHK house. Within the room, there's a small hall with a mounted TV, a kitchen setup for making tea or coffee, and a bedroom with lockers and a study table. They offer many features within the room. The staff is also very friendly and accommodating. Despite not knowing Hindi, I didn't face any difficulties in requesting services as the staff understands our thoughts well. I highly recommend staying here if you're in the Baner area of Pune
+          "
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Vaishnavi Kakade</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Emerald Vista is a hotel situated in baner, Pune. It has spacious 1 rk service apartments which are loaded with facilities like fridge, microwave, electrical kettle, AC etc. The staff is friendly, the restaurant serves delicious food in reasonable prices, must visit place.
+          "
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Hasti</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="I had a great stay at Emerald Vista! The host responded very quickly and was very accommodating to my requests. Check-in instructions were super detailed and helpful. 
+            The apartment was very clean and well stocked . I could not have asked for better location. Overall a wonderful place."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Radhey, Pune, India</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="The rooms are immaculate, the service is great and so is food. The best part is that the owner is very warm and extremely helpful person."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Jens, Berlin, Germany</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="The place is great to stay as it like a little flat. Basic food for breakfast, lunch and dinner is available. The room was clean and everything you need available."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Archana</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="It was a great stay @Emrald Vista. Great Hospitality, the food was too good, special thanks to Mr. Shiva and team, they all were on their toes and gave us a very good service.
+            thanks a lot!"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Deepti</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="This place is good for short trip. Though the location was bit off-track for us, but we liked the entire vibe of place. Complete value for money. Food is better than any hotel- fresh and tasty. I will rate it 4 star for cleanliness and 5 star for food and people.
+            Overall I will recommend this place to anyone who’s looking for a good, peaceful, budgetary place to spend a weekend."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Abhay</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Amazing airbnb. One of the cosiest places in Pune for stay. Checkin process was smooth, the place was really clean, outside food was allowed, the place was small but the owner knows how to make use os space and did a fantastic job of making this airbnb.
+            As a complimentery service, they provided milk powder and coffee sachets(which all Airbnb’s must have, as a service)"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Nitin Shetty</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Unbelievable luxury at such a budget. Extremely clean n hygienic room, well furnished and maintained."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Akshay Agrawal</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Everything was good, I had been there 2nd time and always it has been the best stay of pune no issue nothing"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Kristen, New York, NY</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Very nice, no-frills, multi-unit building with independent suites. Accommodating staff and very communicative and easy to reach host. Great price! Loved having the private patio and little kitchenette."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Sachin Kabade</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="I’m glad to hear you had a positive experience! consider leaving a review like this: I had a wonderful stay! the staff was helpful and cooperative. The hotel’s Location is ideal. Overall, a nice a good experience."
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+        <Card>
+          <div className="text-xl">Torour, Aarhus, Denmark</div>
+          <div className="star">⭐⭐⭐⭐⭐</div>
+          <br />
+          <ReadMore
+            text="Its a great place away from all the noise in the big city. 
+            The host is very friendly and helpful.
+            Highly recommended!"
+          />
+          Rooms 5.0 Service 5.0 Location 5.0
+        </Card>
+
+      
+      </div>
       </div>
     </div>
   );
@@ -70,7 +338,7 @@ interface CardProps {
 }
 const Card: React.FC<CardProps> = (props) => {
   return (
-    <div className="card max-w-[350px] w-max p-4 md:p-6 lg:p-10 rounded-2xl m-2 bg-[#2B0738]">
+    <div className={`card max-w-[350px] w-max p-4 lg:p-8 rounded-2xl m-2 bg-[#2B0738] ${styles.slide} `}>
       {props.children}
     </div>
   );
