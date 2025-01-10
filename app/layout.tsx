b7baccf492b34423ff4d5c8581f3cdc96a6361c6
Om Shejul (Laptop) <contact@omshejul.com>
2025-01-10 22:32:07 +0530

    Integrate Vercel Analytics and Google Tag Manager in RootLayout; update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
+
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +18,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
+          `}
+        </Script>
+      </head>
       <body className={montserrat.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
