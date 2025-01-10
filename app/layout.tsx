cc99228a06228e2d0c18a096b96fcd3b7399a3ab
Om Shejul (Laptop) <contact@omshejul.com>
2025-01-10 22:21:00 +0530

    Remove Google Tag Manager integration from RootLayout component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
-import Head from 'next/head'
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,22 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{
-          __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MX94TFKR');
-          `
-        }} />
-        {/* End Google Tag Manager */}
-      </Head>
       <body className={montserrat.className}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX94TFKR"
-          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         {children}
       </body>
     </html>
